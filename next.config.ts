c3ec8fc7a49bfbca41b01c271454b527e2faecbc
Tayen15 <tiantiantitik@gmail.com>
2025-06-29 16:01:26 +0700

    Enhance Next.js configuration with experimental turbo rules and webpack fallback settings

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,27 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    turbo: {
+      rules: {
+        '*.svg': {
+          loaders: ['@svgr/webpack'],
+          as: '*.js',
+        },
+      },
+    },
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        canvas: false,
+        encoding: false,
+      };
+    }
+    return config;
+  },
+  transpilePackages: ['page-flip'],
 };
 
 export default nextConfig;
